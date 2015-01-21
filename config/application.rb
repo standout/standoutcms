fa10323f98bbef4b8c468aabc180cac1840bf8de
Albert Arvidsson <albert.arvidsson@gmail.com>
2015-01-21 14:07:52 +0100

    Enforce available locales true
    [deprecated] I18n.enforce_available_locales will default to true in the
    future. If you really want to skip validation of your locale you can set
    I18n.enforce_available_locales = false to avoid this message.

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module StandoutCms
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :sv
     config.session_store = :active_record_store
     config.session = {
